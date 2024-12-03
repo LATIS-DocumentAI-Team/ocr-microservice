fd682341a195564b529baba6876e01bd293fe450
HamzaGbada <hamza.gbada@gmail.com>
2024-12-03 18:21:07 +0100

    #2 docker

--- src/controller.py
+++ src/controller.py
@@ -8,10 +8,22 @@ from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 from src.ResponseModel import ResponseModel
 from src.utils import save_file_to_tmp, delete_file
 
-app = FastAPI()
+app = FastAPI(
+    title="OCR API",
+    description="This API applies OCR (Optical Character Recognition) to the provided file and returns the result in a JSON format. "
+    "The recognized text is processed and returned as a Document Object following the DocumentAI-std library format.",
+    version="1.0.0",
+)
 
 
-@app.post("/applyOcr/")
+@app.post(
+    "/applyOcr/",
+    tags=["OCR Processing"],
+    summary="Apply OCR to the provided file",
+    description="This endpoint allows users to apply OCR to an uploaded file. You can specify the OCR method and supported languages. "
+    "The recognized text is processed and returned as a JSON object following the Document Object format "
+    "from the `DocumentAI-std` library (https://pypi.org/project/DocumentAI-std/).",
+)
 async def apply_ocr(
     ocr_method: str = Query(
         default="tesseract",
@@ -19,12 +31,18 @@ async def apply_ocr(
         "for Tesseract, `ocr_method = tesseract`, for EasyOCR, `ocr_method = easy`",
     ),
     languages: List[str] = Query(
-        default= ["en"],
+        default=["en"],
         description="List of supported languages. Supported languages are `fr` (French), `en` (English), `de` (German), `ar` (Arabic), `ja` (Japanese), `ch_sim` (Chinese Simplified), and `hi` (Hindi)"
         "Note: Paddle OCR accept only one language (not a list of languages).",
     ),
     file: UploadFile = File(...),
 ):
+    """
+    This API endpoint applies OCR (Optical Character Recognition) on the provided file.
+    The recognized text is processed and returned as a JSON object following the Document Object format
+    from the `DocumentAI-std` library (https://pypi.org/project/DocumentAI-std/).
+    The Document Object includes various fields such as the recognized text, entities, and document structure.
+    """
     valid_languages = ["fr", "en", "de", "ar", "ja", "ch_sim", "hi"]
 
     valid_extensions = ["jpg", "jpeg", "png", "gif", "bmp", "tiff"]
